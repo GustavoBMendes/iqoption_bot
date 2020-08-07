b6b92ef109e8ba7166abbe98f2768ba19acbd36d
GustavoBMendes <mendes.gustavob@gmail.com>
2020-08-07 15:50:57 -0300

    Capturando velas

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import time, json
 from iqoptionapi.stable_api import IQ_Option
 from datetime import datetime
+from dateutil import tz
 
 API = IQ_Option('email', 'password')
 API.connect()
@@ -14,7 +15,6 @@ if(API.check_connect() == False):
 else:
 	print('Conectado com sucesso')
 
-time.sleep(1)
 
 def perfil():
 	perfil = json.loads(json.dumps(API.get_profile()))
@@ -25,11 +25,43 @@ def timestamp_converter(x):
 	hora = datetime.strptime(datetime.utcfromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
 	hora = hora.replace(tzinfo=tz.gettz('GMT'))
 	
-	return str(hora)[:-6]
+	return str(hora.astimezone(tz.gettz('America/Sao Paulo')))[:-6]
 
-x = perfil()
+def banca():
+	return API.get_balance()
 
-print(x['name'])
-print(x['nickname'])
-print(x['currency'])
-print(API.get_balance())
+def capturarVelas(par, tempo_vela, num_velas):
+	hora = time.time()
+	if(num_velas <= 1000):
+		velas = API.get_candles(par, tempo_vela, num_velas, hora)
+		for vela in velas:
+			print('Horário de início: ' + str(timestamp_converter(vela['from'])) + ' valor de abertura: ' + str(vela['open']))
+
+	else:
+		total = []
+		if(num_velas <= 9999):
+			for i in range( int(round((num_velas/1000), 0)) ):
+				X = API.get_candles(par, tempo_vela, 1000, hora)
+				total = X + total
+				hora = int(X[0]['from']) - 1
+			
+			for velas in total:
+				print(timestamp_converter(velas['from']))
+		else:
+			print('Número de velas igual ou superior a 10000! Entre com um valor menor.')
+
+def capturarVelas_realTime(par, tempo_vela):
+
+	API.start_candles_stream(par, tempo_vela, 1)
+	time.sleep(1)
+
+	while True:
+		vela = API.get_realtime_candles(par, tempo_vela)
+		for valores in vela:
+			print(vela[valores]['close'])
+		time.sleep(1)
+	API.stop_candles_stream(par, tempo_vela)
+
+capturarVelas('EURGBP', 300, 2)
+
+capturarVelas_realTime('EURUSD-OTC', 60)
